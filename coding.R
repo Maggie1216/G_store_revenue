67596f8c320703bb3a0ce6d6c47b49879b2c367f
Maggie1216 <30410684+Maggie1216@users.noreply.github.com>
2018-10-06 14:22:52 +0800

    Almost Ready for Modeling 1006
    Updates:
    -EDA(not done yet);
    -Units of y + typeof cleaning;
    -Some adjustments on the importing order of libraries...

--- coding.R
+++ coding.R
@@ -1,5 +1,7 @@
 #20180907 Data load
 library(readr)
+library(tidyr)
+library(dplyr)
 test<-read_csv("D:\\Maggie\\nk university\\kaggle_g_store\\all\\test.csv")
 train<-read_csv("D:\\Maggie\\nk university\\kaggle_g_store\\all\\train.csv")
 #discription:
@@ -17,16 +19,9 @@ train<-read_csv("D:\\Maggie\\nk university\\kaggle_g_store\\all\\train.csv")
 # visitStartTime - The timestamp (expressed as POSIX time).
 
 #20180920 Cleaning Data
-library(jsonlite)
-library(tidyr)
-library(dplyr)
-library(purrr)
-library(lubridate)
-library(VIM)
-library(mice)
-library(nnet)
 
 #1. datetime format cleaning
+library(lubridate)
 train[[12]]<-as.POSIXlt(train[[12]],origin="1970-01-01")
 test[[12]]<-as.POSIXlt(test[[12]],origin="1970-01-01")
 train[[2]]<-ymd(train[[2]])
@@ -73,13 +68,14 @@ trainy=train[index,]
 #   }
 #   return(df)
 # }
-
+library(jsonlite)
+library(purrr)#transpose
 #jsontrans3, my newest version!
 jsontrans3<-function(dat,columnindex){
   all<-lapply(dat[[columnindex]],fromJSON)
   df<-data.frame()
   for(i in 1:length(all)){
-    df<-union_all(df,data.frame(transpose(all[[i]]),stringsAsFactors = FALSE))
+    df<-union_all(df,data.frame(purrr::transpose(all[[i]]),stringsAsFactors = FALSE))#注意不要让函数覆盖！！
   }
   return(df)
 }
@@ -95,6 +91,9 @@ traintotals<-jsontrans3(trainy,8)
 traintrafficSource<-jsontrans3(trainy,9)
 
 #4. Dealing with NA
+library(VIM)
+library(mice)
+library(nnet)
 #recognizing na
 trainy[c(3,5,8,9)]<-NULL
 sum(trainy[-c(2,8)]=="not available in demo dataset")#there's no such nas in trainy, so we can ignore them
@@ -107,6 +106,8 @@ trainyall<-cbind(trainy,makena(dflst))
 trainyall[,"newVisits"]<-as.numeric(as.character(trainyall[,"newVisits"]))#un-factor this column
 trainyall[,"newVisits"]<-replace(trainyall[,"newVisits"],is.na(trainyall[,"newVisits"]),0)
 trainyall[,"isTrueDirect"]<-replace(trainyall[,"isTrueDirect"],is.na(trainyall[,"isTrueDirect"]),FALSE)
+trainyall[,"bounces"]<-as.numeric(as.character(trainyall[,"bounces"]))#un-factor this column
+trainyall[,"bounces"]<-replace(trainyall[,"bounces"],is.na(trainyall[,"bounces"]),0)
 #deleting na
 missing<-aggr(trainyall,plot = FALSE)[[5]]#missing values count in each column
 deletenalst<-function(alldf,missingdf,tol=1){
@@ -117,7 +118,9 @@ deletenalst<-function(alldf,missingdf,tol=1){
 dellist<-deletenalst(trainyall,missing,0.15)#
 trainyall[,dellist]<-NULL
 #filling na-rest
-
+trainyall[is.na(trainyall$country),"country"]<-"United States"#fill in mode value
+trainyall[is.na(trainyall$subContinent),"subContinent"]<-"Northern America"
+trainyall[is.na(trainyall$continent),"continent"]<-"Americas"
 
 
 #5.Dealing with repetitive values
@@ -125,3 +128,83 @@ trainyall["uniqueid"]<-paste(trainyall$fullVisitorId,trainyall$sessionId)
 trainyall[trainyall[['uniqueid']]=='2571951630476198714_1472105745',]
 trainyall<-trainyall[-5812,]
 trainyall[,"uniqueid"]<-NULL
+
+
+#6. More cleaning: the class of each column should be correct
+#(with no factors before making them into dummy)
+classlist<-lapply(trainyall,class)
+classdf<-data.frame(classlist,stringsAsFactors = FALSE)
+classdf<-rbind(classdf,colnames(classdf))
+classdf<-transpose(classdf)
+colnames(classdf)<-c("class","col")
+classdf#see the class of trainyall columns
+for (i in c("browser","operatingSystem","deviceCategory","continent","subContinent","country","source")){
+  trainyall[,i]<-as.character(trainyall[[i]])
+}#turn them into character
+for (i in c("visits","hits","pageviews","transactionRevenue","newVisits")){
+  trainyall[,i]<-as.numeric(as.character(trainyall[[i]]))
+}#turn them into integer
+
+#7. Correcting the range of y values
+#https://www.kaggle.com/c/ga-customer-revenue-prediction/discussion/65775
+trainyall["transactionRevenue"]<-trainyall["transactionRevenue"]/10^6
+
+#7. EDA
+library(ggplot2)
+#for continuous variables
+cols<-classdf[classdf$class=="numeric","col",drop=T]
+boxplot(trainyall[,cols[c(2,3)]],horizontal = TRUE)
+boxplot(trainyall[,cols[4]],horizontal = TRUE)
+hist(trainyall[,cols[2]])
+hist(trainyall[,cols[3]])
+hist(trainyall[,cols[4]])
+#outliers detection
+otht<-trainyall[,cols[2]]
+otpv<-trainyall[,cols[3]]
+oty<-trainyall[,cols[4]]
+stotht<-(otht-mean(otht))/sd(otht)
+mean(abs(stotht)>3)
+stotpv<-(otpv-mean(otpv))/sd(otpv)
+mean(abs(stotpv)>3)
+stoty<-(oty-mean(oty))/sd(oty)
+mean(abs(stoty)>3)
+#time series
+#revenue by date
+transbydate<-trainyall %>% select(c(date,transactionRevenue)) %>% group_by(date) %>% summarise(sr=sum(transactionRevenue))
+p1<-ggplot(aes(date, sr),data=transbydate)
+p1+geom_line()+geom_smooth()
+#visits by date
+vstbydate<-trainyall %>% select(c(date,visits)) %>% group_by(date) %>% summarise(sr=sum(visits))
+p2<-ggplot(aes(date, sr),data=vstbydate)
+p2+geom_line()+geom_smooth()
+#views by date
+vwbydate<-trainyall %>% select(c(date,pageviews)) %>% group_by(date) %>% summarise(sr=sum(pageviews))
+p3<-ggplot(aes(date, sr),data=vwbydate)
+p3+geom_line()+geom_smooth()
+#hits by date
+htbydate<-trainyall %>% select(c(date,hits)) %>% group_by(date) %>% summarise(sr=sum(hits))
+p3<-ggplot(aes(date, sr),data=htbydate)
+p3+geom_line()+geom_smooth()
+#corr
+con_data<-trainyall%>%select(c(transactionRevenue,pageviews,hits))
+#corr:pageviews and y
+ggplot(aes(pageviews,transactionRevenue),data=con_data)+geom_point()
+ggplot(aes(pageviews,transactionRevenue),data=con_data)+geom_point()+xlim(0,125)+ylim(0,2000)
+cor(otpv,oty)
+#corr:hits and y
+ggplot(aes(hits,transactionRevenue),data=con_data)+geom_point()
+ggplot(aes(hits,transactionRevenue),data=con_data)+geom_point()+xlim(0,125)+ylim(0,2000)
+cor(otht,oty)
+#corr:hits and pageviews
+ggplot(aes(hits,pageviews),data=con_data)+geom_point()
+cor(otht,otpv)
+#corr: pageviews and log(y+1)
+ggplot(aes(pageviews,log(transactionRevenue+1)),data=con_data)+geom_point()
+cor(otpv,log(oty+1))
+#corr: hits and log(y+1)
+ggplot(aes(hits,log(transactionRevenue+1)),data=con_data)+geom_point()
+cor(otht,log(oty+1))
+#distribution
+#distribution: log(y+1)
+qqnorm(log(oty+1))#after log, y is normal
+hist(log(oty+1))
